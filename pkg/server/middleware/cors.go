0ab4bd880c357ecd5f30aa1ed1f10ccc9a428c62
Rafael <rafaelberenguel15@gmail.com>
2025-08-07 23:58:31 -0300

    Update CORS middleware to allow the vercel deployment to interact with our app

--- pkg/server/middleware/cors.go
+++ pkg/server/middleware/cors.go
@@ -8,7 +8,7 @@ import (
 
 func CorsMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://cosmos-ui-iota.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
