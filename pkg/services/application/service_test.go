65f633fd13102654d6929f0956a902828ebc13b3
Rafael <rafaelberenguel15@gmail.com>
2025-08-21 23:46:01 -0300

    SV-008: Adding test for adding an application

--- pkg/services/application/service_test.go
+++ pkg/services/application/service_test.go
@@ -16,6 +16,7 @@ func TestAddApplication(t *testing.T) {
 	t.Run("add application - success", addApplicationSuccess)
 	t.Run("add application - no team success", addApplicationNoTeamSuccess)
 	t.Run("add application - invalid team error", addApplicationInvalidTeamError)
+	t.Run("add application - insert application error", addApplicationInsertApplicationError)
 }
 
 type mocks struct {
@@ -112,4 +113,40 @@ func addApplicationInvalidTeamError(t *testing.T) {
 	err := applicationService.AddApplication(context.Background(), applicationName, applicationDescription, applicationTeam)
 	require.Error(t, err)
 	require.True(t, strings.Contains(err.Error(), "team not found"))
+}
+
+func addApplicationInsertApplicationError(t *testing.T) {
+	applicationService, mocks := setUp(t)
+
+	applicationName := "test-application"
+	applicationDescription := "test-description"
+	applicationTeam := "test-team"
+
+	objTeam := &obj.Team{
+		CosmosObj: obj.CosmosObj{
+			ID: 1,
+		},
+		Name:        applicationName,
+		Description: applicationDescription,
+	}
+
+	teamID := int(objTeam.CosmosObj.ID)
+
+	applicationObj := &obj.Application{
+		Name:        applicationName,
+		Description: applicationDescription,
+		TeamID:      &teamID,
+	}
+
+	mocks.storageServiceMock.EXPECT().
+		GetTeamWithName(gomock.Any(), applicationTeam).
+		Return(objTeam, nil)
+
+	mocks.storageServiceMock.EXPECT().
+		InsertApplication(gomock.Any(), applicationObj).
+		Return(storage.ErrAlreadyExists)
+
+	err := applicationService.AddApplication(context.Background(), applicationName, applicationDescription, applicationTeam)
+	require.Error(t, err)
+	require.True(t, strings.Contains(err.Error(), "application with name "+applicationName+" already exists"))
 }
