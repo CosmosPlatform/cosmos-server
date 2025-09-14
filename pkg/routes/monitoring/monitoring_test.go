858a123e50f08fc248438b930691b80e9e038b93
Rafael <rafaelberenguel15@gmail.com>
2025-09-13 22:47:11 -0300

    SV-010: Add test for handle application update monitoring internal server errror

--- pkg/routes/monitoring/monitoring_test.go
+++ pkg/routes/monitoring/monitoring_test.go
@@ -21,6 +21,7 @@ import (
 func TestHandleUpdateApplicationMonitoring(t *testing.T) {
 	t.Run("success - update application monitoring", handleUpdateApplicationMonitoringSuccess)
 	t.Run("failure - application not found", handleUpdateApplicationMonitoringApplicationNotFound)
+	t.Run("failure - internal server error", handleUpdateApplicationMonitoringInternalServerError)
 }
 
 type mocks struct {
@@ -130,4 +131,58 @@ func handleUpdateApplicationMonitoringApplicationNotFound(t *testing.T) {
 
 	require.Equal(t, http.StatusNotFound, recorder.Code)
 	require.Equal(t, "application not found", actualResponse.Error)
+}
+
+func handleUpdateApplicationMonitoringInternalServerError(t *testing.T) {
+	router, mocks := setUp(t)
+
+	mockedApplicationName := "test-application"
+	mockedDescription := "Test application description"
+	gitProvider := "github"
+	gitRepositoryOwner := "test-owner"
+	gitRepositoryName := "test-repo"
+	gitRepositoryBranch := "main"
+
+	modelApplication := &model.Application{
+		Name:        mockedApplicationName,
+		Description: mockedDescription,
+		Team:        nil,
+		GitInformation: &model.GitInformation{
+			Provider:         gitProvider,
+			RepositoryOwner:  gitRepositoryOwner,
+			RepositoryName:   gitRepositoryName,
+			RepositoryBranch: gitRepositoryBranch,
+		},
+	}
+
+	mockedError := errors.NewInternalServerError("internal server error")
+
+	mocks.applicationServiceMock.EXPECT().
+		GetApplication(gomock.Any(), mockedApplicationName).
+		Return(modelApplication, nil)
+
+	mocks.monitoringServiceMock.EXPECT().
+		UpdateApplicationInformation(gomock.Any(), modelApplication).
+		Return(mockedError)
+
+	mocks.loggerMock.EXPECT().
+		Infow(gomock.Any(), gomock.Any())
+
+	url := fmt.Sprintf("/monitoring/update/%s", mockedApplicationName)
+
+	request, recorder, err := test.NewHTTPRequest("POST", url, nil)
+	if err != nil {
+		t.Fatalf("Failed to create HTTP request: %v", err)
+	}
+
+	router.ServeHTTP(recorder, request)
+
+	actualResponse := api.ErrorResponse{}
+	err = json.NewDecoder(recorder.Body).Decode(&actualResponse)
+	if err != nil {
+		t.Fatalf("Failed to decode response: %v", err)
+	}
+
+	require.Equal(t, http.StatusInternalServerError, recorder.Code)
+	require.Equal(t, "internal server error", actualResponse.Error)
 }
